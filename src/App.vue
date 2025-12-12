bd7e9fe4d1b8d2bdd90a69c4d7d6979c1d40d6a7
Abdulkader <aabdulakader@gmail.com>
2025-12-11 19:59:15 -0600

    github actions test

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Hello World" />
+  <HelloWorld msg="Hello, Abdulkader" />
 </template>
 
 <style scoped>
