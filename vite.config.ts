e400916f21a7017c079ff42ea0a3f14bff567f29
Abdulkader <aabdulakader@gmail.com>
2025-12-15 12:24:37 -0600

    migrated code from js it ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "/aabdi-static-pages-deploy/",
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
 });
